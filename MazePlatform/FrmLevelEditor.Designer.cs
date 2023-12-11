861e0faef773f403c2f776d4fd25bbb036408dbc
@HalilHocaBilsem <karaarslanhalilbilsem@gmail.com>
2023-12-11 20:36:58 +0300

    Proje dosyası ekle.

---
+++ MazePlatform/FrmLevelEditor.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace MazePlatform
+{
+    partial class FrmLevelEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLevelEditor));
+            this.btnNewMap = new System.Windows.Forms.Button();
+            this.btnOpenMap = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnGrass = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btnDanger = new System.Windows.Forms.Button();
+            this.btnQuestion = new System.Windows.Forms.Button();
+            this.btnFinish = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panelMap = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnNewMap
+            // 
+            this.btnNewMap.Location = new System.Drawing.Point(12, 12);
+            this.btnNewMap.Name = "btnNewMap";
+            this.btnNewMap.Size = new System.Drawing.Size(114, 49);
+            this.btnNewMap.TabIndex = 1;
+            this.btnNewMap.Text = "Yeni Harita";
+            this.btnNewMap.UseVisualStyleBackColor = true;
+            this.btnNewMap.Click += new System.EventHandler(this.btnNewMap_Click);
+            // 
+            // btnOpenMap
+            // 
+            this.btnOpenMap.Location = new System.Drawing.Point(132, 12);
+            this.btnOpenMap.Name = "btnOpenMap";
+            this.btnOpenMap.Size = new System.Drawing.Size(114, 49);
+            this.btnOpenMap.TabIndex = 2;
+            this.btnOpenMap.Text = "Harita Aç";
+            this.btnOpenMap.UseVisualStyleBackColor = true;
+            this.btnOpenMap.Click += new System.EventHandler(this.btnOpenMap_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(252, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(114, 49);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Kaydet";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnGrass
+            // 
+            this.btnGrass.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnGrass.BackgroundImage")));
+            this.btnGrass.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGrass.Location = new System.Drawing.Point(442, 12);
+            this.btnGrass.Name = "btnGrass";
+            this.btnGrass.Size = new System.Drawing.Size(50, 49);
+            this.btnGrass.TabIndex = 4;
+            this.btnGrass.Tag = "G";
+            this.btnGrass.UseVisualStyleBackColor = true;
+            this.btnGrass.Click += new System.EventHandler(this.btnMap_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(498, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 49);
+            this.button5.TabIndex = 5;
+            this.button5.Tag = "B";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.btnMap_Click);
+            // 
+            // btnDanger
+            // 
+            this.btnDanger.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDanger.BackgroundImage")));
+            this.btnDanger.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDanger.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDanger.Location = new System.Drawing.Point(610, 12);
+            this.btnDanger.Name = "btnDanger";
+            this.btnDanger.Size = new System.Drawing.Size(50, 49);
+            this.btnDanger.TabIndex = 7;
+            this.btnDanger.Tag = "D";
+            this.btnDanger.UseVisualStyleBackColor = true;
+            this.btnDanger.Click += new System.EventHandler(this.btnMap_Click);
+            // 
+            // btnQuestion
+            // 
+            this.btnQuestion.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnQuestion.BackgroundImage")));
+            this.btnQuestion.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnQuestion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuestion.Location = new System.Drawing.Point(554, 12);
+            this.btnQuestion.Name = "btnQuestion";
+            this.btnQuestion.Size = new System.Drawing.Size(50, 49);
+            this.btnQuestion.TabIndex = 6;
+            this.btnQuestion.Tag = "Q";
+            this.btnQuestion.UseVisualStyleBackColor = true;
+            this.btnQuestion.Click += new System.EventHandler(this.btnMap_Click);
+            // 
+            // btnFinish
+            // 
+            this.btnFinish.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnFinish.BackgroundImage")));
+            this.btnFinish.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnFinish.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFinish.Location = new System.Drawing.Point(723, 12);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(50, 49);
+            this.btnFinish.TabIndex = 9;
+            this.btnFinish.Tag = "F";
+            this.btnFinish.UseVisualStyleBackColor = true;
+            this.btnFinish.Click += new System.EventHandler(this.btnMap_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnStart.BackgroundImage")));
+            this.btnStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Location = new System.Drawing.Point(667, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(50, 49);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Tag = "S";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnMap_Click);
+            // 
+            // panelMap
+            // 
+            this.panelMap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panelMap.Location = new System.Drawing.Point(10, 87);
+            this.panelMap.Name = "panelMap";
+            this.panelMap.Size = new System.Drawing.Size(1300, 600);
+            this.panelMap.TabIndex = 10;
+            // 
+            // FrmLevelEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1397, 695);
+            this.Controls.Add(this.panelMap);
+            this.Controls.Add(this.btnFinish);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnDanger);
+            this.Controls.Add(this.btnQuestion);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnGrass);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpenMap);
+            this.Controls.Add(this.btnNewMap);
+            this.Name = "FrmLevelEditor";
+            this.Text = "FrmLevelEditor";
+            this.Load += new System.EventHandler(this.FrmLevelEditor_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnNewMap;
+        private System.Windows.Forms.Button btnOpenMap;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnGrass;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnDanger;
+        private System.Windows.Forms.Button btnQuestion;
+        private System.Windows.Forms.Button btnFinish;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panelMap;
+    }
+}
---
+++ MazePlatform/FrmLevelEditor.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MazePlatform
+{
+    public partial class FrmLevelEditor : Form
+    {
+        int blokEbat = 50;
+        Image resimCim = null;
+        Image resimCimUyari = null;
+        Image resimStart = null;
+        Image resimFinish = null;
+        Image resimSoru = null;
+        Image resimTugla = null;
+
+        string blokTag = "G";
+
+        int yatayBlokSayisi = 0;
+        int dikeyBlokSayisi = 0;
+
+        int AlininYasi = 10;
+        public FrmLevelEditor()
+        {
+            InitializeComponent();
+     
+
+            
+        }
+
+        private void FrmLevelEditor_Load(object sender, EventArgs e)
+        {
+            resimCim = Image.FromFile("grass.png");
+            resimCimUyari = Image.FromFile("grassDanger.png");
+            resimStart = Image.FromFile("playerStart.png");
+            resimFinish = Image.FromFile("playerFinish.png");
+            resimSoru = Image.FromFile("question.png");
+            resimTugla = Image.FromFile("brick.png");
+
+            panelMap.Size = new Size(1300, 600);
+        }
+
+
+
+        private void btnMap_Click(object sender, EventArgs e)
+        {
+            blokTag = (sender as Button).Tag.ToString();
+        }
+
+        void YeniLevelOlustur()
+        {
+
+            yatayBlokSayisi = panelMap.Width / blokEbat;
+            dikeyBlokSayisi = panelMap.Height / blokEbat;
+            panelMap.Controls.Clear();
+            int blokSol = 0;
+            int blokUst = 0;
+            for (int d = 0; d < dikeyBlokSayisi; d++)
+            {
+                for (int y = 0; y < yatayBlokSayisi; y++)
+                {
+                    PictureBox p = new PictureBox();
+                    p.Tag = "G";
+                    p.Image = resimCim;
+                    p.SizeMode = PictureBoxSizeMode.StretchImage;
+                    p.Size = new Size(blokEbat, blokEbat);
+                    p.Location = new Point(blokSol, blokUst);
+                    p.BorderStyle = BorderStyle.FixedSingle;
+                    panelMap.Controls.Add(p);
+                    p.Click += P_Click;
+                    blokSol += blokEbat;
+                }
+                blokUst += blokEbat;
+                blokSol = 0;
+            }
+        }
+
+        void ResimDegistir(PictureBox p, string yeniTag)
+        {
+            if (yeniTag == "G")
+            {
+                p.Image = resimCim;
+            }
+            else if (yeniTag == "B")
+            {
+                p.Image = resimTugla;
+            }
+            else if (yeniTag == "Q")
+            {
+                p.Image = resimSoru;
+            }
+            else if (yeniTag == "D")
+            {
+                p.Image = resimCimUyari;
+            }
+            else if (yeniTag == "S")
+            {
+                if (BuTagVarMi(yeniTag))
+                {
+                    MessageBox.Show("Başlangıç sadece bir tane olabilir");
+                    return;
+                }
+                p.Image = resimStart;
+            }
+            else if (yeniTag == "F")
+            {
+                if (BuTagVarMi(yeniTag))
+                {
+                    MessageBox.Show("Bitiş sadece bir tane olabilir");
+                    return;
+                }
+                p.Image = resimFinish;
+            }
+            p.Tag = yeniTag;
+        }
+
+        private void P_Click(object sender, EventArgs e)
+        {
+            PictureBox p = sender as PictureBox;
+            ResimDegistir(p, blokTag);
+        }
+
+        bool BuTagVarMi(string tag)
+        {
+
+            foreach (var item in panelMap.Controls)
+            {
+                if (item is PictureBox p)
+                {
+                    if (p.Tag == tag)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void btnNewMap_Click(object sender, EventArgs e)
+        {
+            YeniLevelOlustur();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string haritaMetin = "";
+            int sol = 0;
+            int ust = 0;
+            for (int d = 0; d < dikeyBlokSayisi; d++)
+            {
+                for (int y = 0; y < yatayBlokSayisi; y++)
+                {
+                    var p = KonumaGoreBul(new Point(sol, ust));
+                    haritaMetin += p.Tag.ToString();
+                    sol += blokEbat;
+                }
+                haritaMetin += Environment.NewLine;
+                sol = 0;
+                ust += blokEbat;
+            }
+
+            SaveFileDialog sf=new SaveFileDialog();
+            sf.DefaultExt = ".map";
+            sf.Filter = "Harita Dosyası|*.map";
+            if (sf.ShowDialog()== DialogResult.OK)
+            {
+                File.WriteAllText(sf.FileName, haritaMetin);
+                MessageBox.Show("Harita Kaydedildi");
+            }
+
+        }
+        PictureBox KonumaGoreBul(Point konum)
+        {
+            foreach (var item in panelMap.Controls)
+            {
+                if (item is PictureBox p)
+                {
+                    if (p.Location == konum)
+                    {
+                        return p;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void btnOpenMap_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of=new OpenFileDialog();
+            of.Filter = "Harita Dosyası|*.map";
+            if (of.ShowDialog()== DialogResult.OK)
+            {
+                var haritaMetinSatirlar = File.ReadAllLines(of.FileName);
+
+                panelMap.Controls.Clear();
+
+                int ust = 0;
+                int sol = 0;
+                foreach (var satir in haritaMetinSatirlar)
+                {
+                    foreach (var karakter in satir)
+                    {
+                        PictureBox p = new PictureBox();
+                       
+                        p.Image = resimCim;
+                        p.SizeMode = PictureBoxSizeMode.StretchImage;
+                        p.Size = new Size(blokEbat, blokEbat);
+                        p.Location = new Point(sol, ust);
+                        p.BorderStyle = BorderStyle.FixedSingle;
+                        panelMap.Controls.Add(p);
+                        p.Click += P_Click;
+                        sol += blokEbat;
+                        ResimDegistir(p, karakter.ToString());
+                    }
+                    sol = 0;
+                    ust += blokEbat;
+                }
+               
+            }
+
+        }
+    }
+}
